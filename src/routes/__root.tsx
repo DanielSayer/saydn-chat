7cb692285aca21139f9a775c1e674d7b03eba171
Daniel Sayer <daniel.sayer01@gmail.com>
2025-09-04 22:30:29 +1000

    chore: add favicons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,7 +27,27 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
           title: "saydn chat",
         },
       ],
-      links: [{ rel: "stylesheet", href: globals_css }],
+      links: [
+        { rel: "stylesheet", href: globals_css },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "icon", href: "/favicon.ico" },
+      ],
     }),
     component: RootComponent,
   },
